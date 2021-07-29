77b2f9e681431305185beff8b7ebbf0031f9848f
giov <pintogiovanni818@gmail.com>
2021-07-29 14:28:47 +0200

    modifiche

--- grafica/grafica.js
+++ grafica/grafica.js
@@ -1,20 +1,25 @@
 function creaCampo(celle) {
-    for (let i = 0; i <= celle; i++) {
-        const element = array[i];
+    for (let i = 1; i <= celle; i++) {
+       
         let celle =`<div data-cella ="${i}" class="cella">
         </div>`;
 
         let templateCella = document.createElement("DIV");
         templateCella.classList.add("quadrato"); 
-        templateCella.innerHTML = cella;
-        document.getElementById("cella").appendChild(templateCella);
+        templateCella.innerHTML = celle;
+        document.getElementById("campo").appendChild(templateCella);
     }
 }
 
 document.getElementById("campo").addEventListener("click",
-function e() {
-    console.log(e);
+function (e) {
+    console.log(e.target.dataset.cella);
+    console.log(numBombe);
+    if (numBombe.includes(parseInt(e.target.dataset.cella))) {
+        alert("hai perso");
+    }
     
 })
 
 creaCampo(100);
+
